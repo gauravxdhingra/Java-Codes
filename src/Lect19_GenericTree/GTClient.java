a1f6d1721715d1f1c234408528109519531f8637
GauravXd <grvdhingra1999@gmail.com>
2019-10-26 21:23:02 +0530

    Lecture 19

---
+++ src/Lect19_GenericTree/GTClient.java
@@ -0,0 +1,32 @@
+package Lect19_GenericTree;
+
+public class GTClient {
+
+	public static void main(String[] args) {
+
+		// 10 3 20 2 50 0 60 0 30 0 40 2 70 0 80 0
+		// 10 3 20 3 50 2 100 0 110 0 60 0 70 2 120 2 160 0 170 0 130 0 30 0 40 2 80 0 90 2 140 2 180 0 190 0 150 0
+		GenericTree gt = new GenericTree();
+		gt.display();
+
+		// System.out.println(gt.size());
+		// System.out.println(gt.max());
+		// System.out.println(gt.find(60));
+		// System.out.println(gt.ht());
+		// gt.mirror();
+		// gt.display();
+
+		// gt.printAtLevel(2);
+		// gt.linearize();
+		// gt.display();
+		//
+		// gt.preorder();
+		//
+		// gt.postorder();
+
+		// gt.levelOrder();
+		// gt.levelOrderLW2();
+		gt.levelOrderZZ();
+
+	}
+}

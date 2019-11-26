19ffebeaf19ab56da51974a1c4f358f6517237b0
GauravXd <grvdhingra1999@gmail.com>
2019-11-26 19:33:43 +0530

    Lecture 33

---
+++ src/Lect33_DynamicStack/Client.java
@@ -0,0 +1,11 @@
+package Lect33_DynamicStack;
+
+
+public class Client {
+
+	public static void main(String[] args) {
+
+		Lect33_DynamicStack.DS ds = new Lect33_DynamicStack.DynamicStack();
+
+	}
+}
---
+++ src/Lect33_DynamicStack/Stack.java
@@ -0,0 +1,6 @@
+package Lect33_DynamicStack;
+
+public abstract class Stack extends Lect33_DynamicStack.DS {
+
+	public abstract int pop();
+}
